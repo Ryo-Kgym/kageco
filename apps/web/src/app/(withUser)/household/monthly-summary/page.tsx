ad235308c1ba7cfc5c06722747c5ebc943881d56
Ryo-Kgym <mraytoh@icloud.com>
2025-03-15 23:05:43 +0900

    One-for-All-260 デフォルトの日付を設定し、リダイレクト機能を追加

--- apps/web/src/app/(withUser)/household/monthly-summary/page.tsx
+++ apps/web/src/app/(withUser)/household/monthly-summary/page.tsx
@@ -1,6 +1,10 @@
+import { redirect } from "next/navigation";
 import { type YYYY_MM_DD, YYYYmmDD } from "@/type/date/date";
+import { convertToYmd } from "@/core/function/date/convertToYmd";
 
 import { MonthlySummaryServer } from "../../../../features/householdMonthlySummary/components/MonthlySummaryServer";
+import { getPast12MonthYyyyMMdd } from "../../../../function/date/getPast12MonthYyyyMMdd";
+import { paths } from "../../../../routing/paths";
 
 const Page = async ({
   searchParams,
@@ -11,6 +15,18 @@ const Page = async ({
   }>;
 }) => {
   const { from, to } = await searchParams;
+
+  // fromDateとtoDateがない場合は、デフォルト値を設定してリダイレクト
+  if (!from || !to) {
+    const defaultFromDate = getPast12MonthYyyyMMdd();
+    const defaultToDate = new YYYYmmDD(convertToYmd(new Date()));
+    
+    const fromParam = from || defaultFromDate.toString();
+    const toParam = to || defaultToDate.toString();
+    
+    const redirectUrl = `${paths.household.monthlySummary.root()}?from=${fromParam}&to=${toParam}`;
+    redirect(redirectUrl);
+  }
 
   const convertToDate = (date: YYYY_MM_DD | undefined) => {
     return date ? new YYYYmmDD(date) : undefined;
