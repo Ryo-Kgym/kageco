76ccfbedd9907f9060b7d7a7bad3b6223a22f517
Ryo-Kgym <mraytoh@icloud.com>
2025-04-12 15:57:17 +0900

    kageco-269 予実に関する項目を追加した。

--- apps/web/src/features/businessTimeCard/components/MonthlySummary.tsx
+++ apps/web/src/features/businessTimeCard/components/MonthlySummary.tsx
@@ -1,26 +1,51 @@
 import type { FC } from "react";
 
 import { convertSecondToHour } from "../../../function/date/convertSecond";
+import styles from "./MonthlySummary.module.scss";
 
 type Props = {
+  businessDays: number;
+  planWorkSecondLower: number;
+  planWorkSecondUpper: number;
   totalWorkSecond: number;
+  remainingWorkSecondLower: number;
+  recommendedDailyWorkSecond: number;
 };
 
-export const MonthlySummary: FC<Props> = ({ totalWorkSecond }) => {
+export const MonthlySummary: FC<Props> = ({
+  businessDays,
+  planWorkSecondLower,
+  planWorkSecondUpper,
+  totalWorkSecond,
+  remainingWorkSecondLower,
+  recommendedDailyWorkSecond,
+}) => {
   return (
-    <table
-      style={{
-        width: "100%",
-      }}
-    >
-      <thead>
-        <tr>
-          <th>月間勤務時間</th>
-        </tr>
-      </thead>
+    <table className={styles.table}>
       <tbody>
         <tr>
-          <td align={"center"}>{convertSecondToHour(totalWorkSecond).hhmm}</td>
+          <th>営業日数</th>
+          <td>{businessDays}</td>
+        </tr>
+        <tr>
+          <th>予定総労働時間（下限）</th>
+          <td>{convertSecondToHour(planWorkSecondLower).hhmm}</td>
+        </tr>
+        <tr>
+          <th>予定総労働時間（上限）</th>
+          <td>{convertSecondToHour(planWorkSecondUpper).hhmm}</td>
+        </tr>
+        <tr>
+          <th>実績総労働時間</th>
+          <td>{convertSecondToHour(totalWorkSecond).hhmm}</td>
+        </tr>
+        <tr>
+          <th>下限までの残り労働時間</th>
+          <td>{convertSecondToHour(remainingWorkSecondLower).hhmm}</td>
+        </tr>
+        <tr>
+          <th>残りの1日あたりの労働時間</th>
+          <td>{convertSecondToHour(recommendedDailyWorkSecond).hhmm}</td>
         </tr>
       </tbody>
     </table>
