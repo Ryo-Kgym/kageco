758a7625e4a511ff54089e4a78c780dc0a38c949
Ryo-Kgym <mraytoh@icloud.com>
2025-05-06 22:56:40 +0900

    kageco-276 クレジットカード詳細編集モーダルにタブコンポーネントを追加し、UIを変更した。

--- apps/web/src/features/householdCreditDetailEdit/components/CreditCardDetailEditModel.tsx
+++ apps/web/src/features/householdCreditDetailEdit/components/CreditCardDetailEditModel.tsx
@@ -1,6 +1,7 @@
 import type { ComponentProps, FC } from "react";
 
 import { Modal } from "../../../components/atoms/Modal";
+import { Tab } from "../../../components/ui";
 import { CreditCardDetailEditContainer } from "./CreditCardDetailEditContainer";
 
 type Props = ComponentProps<typeof CreditCardDetailEditContainer> & {
@@ -15,7 +16,19 @@ export const CreditCardDetailEditModal: FC<Props> = ({
 }) => {
   return (
     <Modal opened={isOpen} onClose={onCloseHandler}>
-      <CreditCardDetailEditContainer id={id} onClose={onCloseHandler} />
+      <Tab
+        defaultSelect={"creditCard"}
+        tabPropsList={[
+          {
+            value: "creditCard",
+            label: "変更",
+            icon: null,
+            contents: (
+              <CreditCardDetailEditContainer id={id} onClose={onCloseHandler} />
+            ),
+          },
+        ]}
+      />
     </Modal>
   );
 };
