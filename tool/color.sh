0aaaf35f37c5cf88607e34ed06e7172fba8e3fcf
Ryo-Kgym <mraytoh@icloud.com>
2025-10-25 23:11:18 +0900

    kageco-275 環境初期化スクリプトの追加を実施 ・プロジェクトの環境初期化を簡略化するための`init-env.sh`スクリプトを新規作成した。 ・Dockerコンテナおよび`node_modules`の削除と再構築を対話形式および引数指定で実行可能にした。 ・ログ出力用の関数や色指定変数を格納する`tool/function.sh`および`tool/color.sh`を追加し、コードの再利用性を向上させた。

---
+++ tool/color.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Reset
+export Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+export Black='\033[0;30m'        # Black
+export Red='\033[0;31m'          # Red
+export Green='\033[0;32m'        # Green
+export Yellow='\033[0;33m'       # Yellow
+export Blue='\033[0;34m'         # Blue
+export Purple='\033[0;35m'       # Purple
+export Cyan='\033[0;36m'         # Cyan
+export White='\033[0;37m'        # White
