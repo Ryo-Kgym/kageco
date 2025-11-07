c454b3c040599f62fceab168df53fe0f32e0ad7d
Ryo-Kgym <mraytoh@icloud.com>
2025-11-08 02:30:19 +0900

    kageco-301 tRPCアーキテクチャ初期設定およびサーバー起動ロジックの調整を実施 ・新規に`tRPC`関連ファイル（`server.ts`）を追加し、`User`および`Group`ルーターの初期設定を実装した。 ・`main.ts`で環境変数を使用したポート設定に変更し、柔軟なサーバー起動を実現した。 ・Reflect-metadataのインポートを追加し、デコレーター使用の基盤を整備した初期設定である。

--- packages/trpc/@generated/server.ts
+++ packages/trpc/@generated/server.ts
@@ -5,8 +5,11 @@ const t = initTRPC.create();
 const publicProcedure = t.procedure;
 
 const appRouter = t.router({
-  user: t.router({ getUser: publicProcedure.query(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any) }),
-  group: t.router({ getGroupsMembers: publicProcedure.query(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any) })
+  user: t.router({
+    getUser: publicProcedure.query(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any),
+  }),
+  group: t.router({
+    getGroupsMembers: publicProcedure.query(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any),
+  }),
 });
 export type AppRouter = typeof appRouter;
-
